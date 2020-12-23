1a684d82212c258dcd07efe9277bb7e03b46686f
dimahike <dimahike@gmail.com>
2020-12-23 16:53:11 +0200

    implement Signin bacend

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -25,15 +25,10 @@ productRouter.get(
 productRouter.get(
   '/:id',
   expressAsyncHandler(async (req, res) => {
-    console.log('work 1');
     const product = await Product.findById(req.params.id);
-    console.log('work 2');
     if (product) {
-      console.log('work 3');
       res.send(product);
-      console.log('work 4');
     } else {
-      console.log('work 5');
       res.status(404).send({ message: 'Product Not Found' });
     }
   }),
